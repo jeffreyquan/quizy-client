aedec2579e516daba803ec0ce66f450251fbf010
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-11 00:24:17 +1100

    started populating quiz components

---
+++ src/components/quiz/Quiz.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+let URL = ( model, id = '') => {
+  return `https://`
+};
+
+
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: null
+    }
+  }
+}

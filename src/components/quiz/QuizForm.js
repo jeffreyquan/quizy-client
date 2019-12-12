ac3c8239fcfa2d96edae9e1046bfe909b94ceb6c
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-12 20:00:27 +1100

    updated question form

--- src/components/quiz/QuizForm.js
+++ src/components/quiz/QuizForm.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 let URL = ( model, id = '') => {
@@ -45,6 +44,7 @@ class QuizForm extends Component {
         <form>
           <input type="text" name="name" placeholder="Quiz Name" value={ this.state.name } onChange={ this._handleChange } required />
           <input type="text" name="category" placeholder="Category" value={ this.state.category } onChange={ this._handleChange } required />
+
         </form>
         <QuestionForm onSubmit={ this.saveQuestion }/>
         <button onClick={() => this.addQuestion() }>Add Question</button>
@@ -58,50 +58,41 @@ class QuestionForm extends Component {
     super();
     this.state = {
       body: '',
-      answers: [
-        {
-          body: ''
-        },
-        {
-          body: ''
-        },
-        {
-          body: ''
-        },
-        {
-          body: ''
-        }
-      ],
+      answers: {
+        'a': '',
+        b: '',
+        c: '',
+        d: ''
+      },
       correct: ''
     };
-
-    this._handleChange = this._handleChange.bind(this);
-    this._handleSubmit = this._handleSubmit.bind(this);
   };
 
-  _handleChange(event) {
-    const { name, value } = event.target
+  _handleChange = (event) => {
+    const { name, value } = event.target;
     this.setState({ [name]: value });
   }
 
-  _handleSubmit(event) {
+  _handleAnswerChange = (event) => {
+    const { name, value } = event.target;
+    this.setState(prevState => ({
+      answers: {
+        ...prevState.answers,
+        [name]: value
+      }
+    }))
+  }
+
+  _handleSubmit = (event) => {
     event.preventDefault();
     this.props.onSubmit(this.state);
     this.setState({
-      answers: [
-        {
-          body: ''
-        },
-        {
-          body: ''
-        },
-        {
-          body: ''
-        },
-        {
-          body: ''
-        }
-      ],
+      answers: {
+        'a': '',
+        b: '',
+        c: '',
+        d: ''
+      },
       correct: ''
     })
   }
@@ -110,14 +101,21 @@ class QuestionForm extends Component {
     return (
       <form onSubmit={ this._handleSubmit } >
         <input type='text' name='body' value={ this.state.body } onChange={ this._handleChange } placeholder="Question" />
-        <label>Answer 1: </label>
-        <input type='text' name='answer' value={ this.state.answers[0].body } required />
-        <label>Answer 2: </label>
-        <input type='text' name='answer' value={ this.state.answers[1].body } required />
-        <label>Answer 3: </label>
-        <input type='text' name='answer' value={ this.state.answers[2].body } required />
-        <label>Answer 4: </label>
-        <input type='text' name='answer' value={ this.state.answers[3].body } required />
+        <label>Answer A: </label>
+        <input type='text' name='a' value={ this.state.answers.a } onChange={ this._handleAnswerChange } required />
+        <label>Answer B: </label>
+        <input type='text' name='b' value={ this.state.answers.b } onChange={ this._handleAnswerChange } required />
+        <label>Answer C: </label>
+        <input type='text' name='c' value={ this.state.answers.c } onChange={ this._handleAnswerChange } required />
+        <label>Answer D: </label>
+        <input type='text' name='d' value={ this.state.answers.d } onChange={ this._handleAnswerChange } required />
+        <label>Correct Answer:</label>
+        <select name="correct" value={ this.state.correct } onChange={ this._handleChange }>
+          <option value="a">A</option>
+          <option value="b">B</option>
+          <option value="c">C</option>
+          <option value="d">D</option>
+        </select>
         <input type='submit' />
       </form>
     )

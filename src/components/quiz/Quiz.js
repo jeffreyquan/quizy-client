f8908f5b0d7c8d5eb365c857f768ceff2bf07a41
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-14 16:14:42 +1100

    created quiz preview show page

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import QuizInfo from '../utils';
+import { Container } from '@material-ui/core';
 
-let URL = ( model, id = '') => {
-  return `https://localhost:3000/${ model }/${ id }`
-};
-
-
-class Quiz extends Component {
-  constructor(props) {
-    super(props);
+export default class Quiz extends Component {
+  constructor() {
+    super();
     this.state = {
       name: '',
       category: '',
@@ -19,20 +15,49 @@ class Quiz extends Component {
   }
 
   componentDidMount() {
+    console.log( this.props.match.params );
     const { quizId } = this.props.match.params;
-
-    QuizInfo.getQuiz( quizId ).then(( { data }) => {
-      console.log(data);
+    console.log( quizId );
+    QuizInfo.getQuiz( quizId ).then(( { data } ) => {
+      const { name, category, questions } = data;
+      console.log(name, category, questions);
+      this.setState({
+        name: name,
+        category: category,
+        questions: questions
+      })
     });
   }
 
   render() {
     return (
-      <div>
-        <h1>Quiz coming soon</h1>
-      </div>
-    )
+      <>
+        <Container maxWidth="md">
+          <h1>Quiz preview</h1>
+          <h2>Quiz: { this.state.name }</h2>
+          <h2>Category: { this.state.category }</h2>
+          <PreviewQuestions questions={ this.state.questions } />
+        </Container>
+      </>
+    );
   }
 }
 
-export default Quiz;
+const PreviewQuestions = (props) => {
+  if (props.questions.length === 0) {
+    return (<div>Questions are loading.</div>)
+  }
+
+  const questions = props.questions.map((q, i) => (
+    <div key={ i }>
+      <div style={{ fontWeight: "bold" }}>Question { i + 1 }</div>
+      <p>{ q.question }</p>
+    </div>
+  ))
+
+  return (
+    <div>
+      { questions }
+    </div>
+  )
+}

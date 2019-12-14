c0ff070497cd21d60a6144ae7600d896d4ebf531
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-15 02:34:24 +1100

    additional route for lobby

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import JoinGame from './Player/JoinGame';
 import QuizForm from './CreateQuiz/QuizForm';
-import Quiz from './Quiz/Quiz'
+import Quiz from './Quiz/Quiz';
+import Lobby from './Lobby/Lobby';
 import { HashRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div>
-        <Route exact path='/quizzes' component={ QuizForm } />
-        <Route path='/quizzes/:quizId' component={ Quiz } />
+        <Route exact path='/' component={ JoinGame }/>
+        <Route exact path='/quizzes' component={ QuizForm }/>
+        <Route path='/lobby' component={ Lobby }/>
+        <Route path='/quizzes/:quizId' component={ Quiz }/>
       </div>
     </Router>
   );

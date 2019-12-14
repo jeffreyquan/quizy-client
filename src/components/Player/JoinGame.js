decfc2d00e4423192575eb207a77cdff8c3cd1ef
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-15 02:33:53 +1100

    initiated players join game components

---
+++ src/components/Player/JoinGame.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import socketIOClient from 'socket.io-client'
+
+export default class JoinGame extends Component {
+  constructor() {
+    super()
+
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Making sure this works</p>
+      </div>
+    )
+  }
+}
---
+++ src/components/Player/NicknameForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { TextField, Button, Grid } from '@material-ui/core';
+
+export default class NicknameForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nickname: ''
+    };
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: '100vh' }}
+        >
+          <Grid item md={4} sm={12}>
+            <TextField margin="dense" name="pin" value={ this.state.pin } onChange={ this.handleChange } variant="outlined" placeholder="Game PIN" fullWidth/>
+          </Grid>
+          <Grid item md={4} sm={12}>
+            <Button variant="contained" color="primary" type="submit" fullWidth>
+              Enter
+            </Button>
+          </Grid>
+          <Grid item md={4} sm={12}>
+            <Link to="/login">Login to create your own quizy</Link>
+          </Grid>
+        </Grid>
+      </form>
+    )
+  }
+}

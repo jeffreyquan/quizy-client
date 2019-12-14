7f2e5942f6c4cab9ea1116170bf45158b3f7151e
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-14 16:11:46 +1100

    added confirmation page for quiz creation which redirects to the quiz preview page

---
+++ src/components/CreateQuiz/Confirm.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { List, ListItem, Grid, Button } from '@material-ui/core';
+
+export default class Confirm extends Component {
+  continue = event => {
+    event.preventDefault();
+    this.props.saveQuiz();
+  };
+
+  back = event => {
+    event.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    const { values: { name, category, questions} } = this.props;
+    return (
+      <>
+        <Grid
+          container
+          direction="column"
+          spacing={2}
+          alignItems="center"
+          justify="center"
+        >
+          <Grid item md={4} sm={12}>
+            <h1>Confirm details:</h1>
+          </Grid>
+          <Grid item md={4} sm={12}>
+            <Grid
+              container
+              alignItems="flex-start"
+            >
+              <Grid item xs={12}>
+                <QuizDetails name={ name } category={ category } />
+                <QuestionList questions={ questions } />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item md={4} sm={12}>
+            <Grid
+              container
+              spacing={2}
+            >
+              <Grid item md={6} xs={12}>
+                <Button variant="contained" color="secondary" onClick={ this.back } fullWidth>
+                  Back
+                </Button>
+              </Grid>
+              <Grid item md={6} xs={12}>
+                <Button variant="contained" color="primary" onClick={ this.continue } fullWidth>
+                  Confirm
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </>
+    )
+  }
+}
+
+const QuizDetails = (props) => {
+  return (
+    <div>
+      <p><span style={{ fontWeight: "bold" }}>Name: </span>{ props.name }</p>
+      <p><span style={{ fontWeight: "bold" }}>Category: </span>{ props.category }</p>
+    </div>
+  )
+}
+
+const QuestionList = (props) => {
+  const quizQuestions = props.questions.map((q,i) => (
+    <div key={ i }>
+      <div style={{ fontWeight: "bold" }}>Question { i + 1 }</div>
+      <p>{ q.question } Answer: { q.answers[q.correct] }</p>
+    </div>
+  ))
+
+  return (
+    <div>
+      { quizQuestions }
+    </div>
+  )
+}

c015c774b0f2cc30ae75846e73c5c9463038e236
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-16 17:07:11 +1100

    attempted to include a game not found message but needs fixing

--- src/components/Player/JoinGame.js
+++ src/components/Player/JoinGame.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { socket } from '../Global/Header';
+import { PLAYER_JOINED, GAME_NOT_FOUND } from '../Events';
 
 export default class JoinGame extends Component {
   constructor() {
@@ -9,8 +10,8 @@ export default class JoinGame extends Component {
     this.state = {
       nickname: '',
       pin: '',
+      status: null
     };
-
   }
 
   handleChange = event => {
@@ -23,15 +24,26 @@ export default class JoinGame extends Component {
   handleSubmit = event => {
     event.preventDefault();
     console.log('Enter button working...');
-    socket.emit('playerJoin', this.state);
+    const { nickname, pin } = this.state;
+    socket.emit(PLAYER_JOINED, { nickname: nickname, pin: pin });
+
+    socket.on(GAME_NOT_FOUND, () => {
+      console.log('Game not found...');
+      this.setState({
+        status: false
+      })
+    });
+
   }
 
-  // socket.on('gameNotFound', () => {
-  //   console.log('Game not found...');
-  // });
-
-
   render() {
+    let message;
+    if ( this.state.status === false ) {
+      message = <div>Game not found</div>;
+    } else {
+      message = <div></div>
+    }
+
     return (
       <div>
         <p>Making sure works</p>
@@ -42,6 +54,7 @@ export default class JoinGame extends Component {
             Enter
           </Button>
         </form>
+        { message }
       </div>
     )
   }

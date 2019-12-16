092675781b1bfb8f94c88af7769377b9c5f24e6f
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-17 02:02:53 +1100

    added instructions, player start and question components to routes

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ import QuizForm from './CreateQuiz/QuizForm';
 import Quiz from './Quiz/Quiz';
 import Lobby from './Host/Lobby';
 import Start from './Host/Start';
+import Question from './Host/Question';
+import Instructions from './Player/Instructions';
+import PlayerStart from './Player/PlayerStart';
 import { Header } from './Global/Header';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
@@ -15,7 +18,10 @@ function App() {
         <Switch>
           <Route exact path='/' component={ JoinGame }/>
           <Route exact path='/quizzes' component={ QuizForm }/>
+          <Route exact path='/instructions' component={ Instructions }/>
           <Route path='/lobby' component={ Lobby }/>
+          <Route path='/question' component={ Question }/>
+          <Route path='/play' component={ PlayerStart }/>
           <Route path='/start' component={ Start }/>
           <Route path='/quizzes/:quizId' component={ Quiz }/>
         </Switch>

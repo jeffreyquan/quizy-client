cef7aff7c6690fa40078b5af02a14a0f859e0f4a
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-18 02:12:03 +1100

    roughly populating results component showing correct answer, no of players with each answer and a next button

--- src/components/Host/ResultBlock.js
+++ src/components/Host/ResultBlock.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import { socket } from '../Global/Header';
+import Button from '@material-ui/core/Button';
 
 export default class ResultBlock extends Component {
-  
+
+
+  onClick = () => {
+    this.props.nextStep();
+    console.log('Heading to scoreboard...');
+  }
 
   render() {
+    const { answers, answeredA, answeredB, answeredC, answeredD, correct } = this.props;
     return (
       <div>
-        Result block coming soon
+        <div>Correct: { correct }</div>
+        <div>{ answeredA } answered A</div>
+        <div>{ answeredB } answered B</div>
+        <div>{ answeredC } answered C</div>
+        <div>{ answeredD } answered D</div>
+        <div>{ answers.a }</div>
+        <div>{ answers.b }</div>
+        <div>{ answers.c }</div>
+        <div>{ answers.d }</div>
+        <Button variant="contained" color="primary" onClick={ this.handleClick }>
+          Next
+        </Button>
       </div>
     )
   }

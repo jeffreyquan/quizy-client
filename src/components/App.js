ae29a245e987434c4fc999baad98a05ef39ca26f
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-17 17:10:12 +1100

    additional routes

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,10 @@ import QuizForm from './CreateQuiz/QuizForm';
 import Quiz from './Quiz/Quiz';
 import Lobby from './Host/Lobby';
 import Start from './Host/Start';
-import Question from './Host/Question';
+import Gameblock from './Host/Gameblock';
+import Playblock from './Player/Playblock';
 import Instructions from './Player/Instructions';
-import Play from './Player/Play';
+import GetReady from './Player/GetReady';
 import { Header } from './Global/Header';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
@@ -19,10 +20,11 @@ function App() {
           <Route exact path='/' component={ JoinGame }/>
           <Route exact path='/quizzes' component={ QuizForm }/>
           <Route exact path='/instructions' component={ Instructions }/>
+          <Route path='/getready' component={ GetReady }/>
           <Route path='/lobby' component={ Lobby }/>
-          <Route path='/question' component={ Question }/>
-          <Route path='/play' component={ Play }/>
           <Route path='/start' component={ Start }/>
+          <Route path='/gameblock' component={ Gameblock }/>
+          <Route path='/playblock' component={ Playblock }/>
           <Route path='/quizzes/:quizId' component={ Quiz }/>
         </Switch>
       </HashRouter>

16a52647006b40cbf155d63dfe58327463c4d421
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-19 01:25:26 +1100

    created basic ranking component -- shows at the end of the game

--- src/components/Player/Ranking.js
+++ src/components/Player/Ranking.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import { socket } from '../Global/Header';
 
-export default class Ranking extends Component {
-
-  componentDidMount() {
-
-  }
-
-  render() {
-    return (
-      <div>
-        Ranking coming soon
-      </div>
-    )
-  }
+function Ranking(props) {
+  return (
+    <div>
+      <Rank rank={ props.rank } />
+      <div>Score: { props.score }</div>
+    </div>
+  )
 }
+
+const Rank = (props) => {
+
+  let finalRank;
+
+  if (props.rank === 1) {
+    finalRank = <div>1st place</div>
+  } else if (props.rank === 2) {
+    finalRank = <div>2nd place</div>
+  } else if (props.rank === 3) {
+    finalRank = <div>3rd place</div>
+  } else {
+    finalRank=<div>{`${props.rank}th place`}</div>
+  }
+
+  return (
+    <div>
+      { finalRank }
+    </div>
+  )
+}
+
+export default Ranking;

739b46ec3d8764269a29383666f1f9f3dfe7461b
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-18 16:45:16 +1100

    switched back to class components instead of useEffect

--- src/components/Player/GetReady.js
+++ src/components/Player/GetReady.js
@@ -1,33 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { socket } from '../Global/Header';
 import { READY } from '../Events';
 
-export default function GetReady(props) {
-  const [nickname, setNickname] = useState(null);
-  const [pin, setPin] = useState(null);
+export default class GetReady extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nickname: null,
+      pin: null
+    };
+  }
 
-  useEffect(() => {
+  componentDidMount() {
     const queryString = require('query-string');
-    const parsed = queryString.parse(props.location.search);
+    const parsed = queryString.parse(this.props.location.search);
     const nickname = parsed.nickname;
     const pin = parsed.pin;
-    console.log('Player joined room with pin:', pin);
-      setNickname(nickname);
-      setPin(pin);
+    console.log('Get ready page for player in room:', pin);
+    this.setState({
+      nickname: nickname,
+      pin: pin
+    })
 
     socket.on(READY, () => {
-      console.log('Player is ready and waiting.');
-      setTimeout(() => {
-        props.history.push(`/playblock?nickname=${ nickname }&pin=${ pin }`);
-        socket.off(READY);
+     console.log('Player ready....');
+     setTimeout(() => {
+        this.props.history.push(`/playblock?nickname=${ this.state.nickname }&pin=${ this.state.pin }`)
       }, 5000);
-    });
-  });
+    })
+  }
 
-  return (
-    <div>
-      <h1>Get ready</h1>
-      <p>Loading...</p>
-    </div>
-  )
+  render() {
+    return(
+      <div>
+        <h1>Get ready</h1>
+        <p>Loading...</p>
+      </div>
+    )
+  }
 }
--- src/components/Player/Instructions.js
+++ src/components/Player/Instructions.js
@@ -1,37 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { socket } from '../Global/Header';
 import Pin from '../Global/Pin';
 import { GAME_HAS_STARTED } from '../Events';
 
-export default function Instructions(props) {
-  const [nickname, setNickname] = useState(null);
-  const [pin, setPin] = useState(null);
-
-  useEffect(() => {
-    const queryString = require('query-string');
-    const parsed = queryString.parse(props.location.search);
-    const nickname = parsed.nickname;
-    const pin = parsed.pin;
-    console.log('Player joined room with pin:', pin);
-      setNickname(nickname);
-      setPin(pin);
-
-
-    socket.on(GAME_HAS_STARTED, () => {
-      props.history.push(`/getready?nickname=${ nickname }&pin=${ pin }`);
-      socket.off(GAME_HAS_STARTED);
-    });
-  });
-
-  if (pin === null) {
-    return 'Loading...';
+export default class Instructions extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nickname: null,
+      pin: null
+    };
   }
 
-  return (
-    <div>
-      <Pin pin={ pin }/>
-      <p>You're in</p>
-      <p>See your nickname on screen?</p>
-    </div>
-  )
+  componentDidMount() {
+    const queryString = require('query-string');
+    const parsed = queryString.parse(this.props.location.search);
+    const nickname = parsed.nickname;
+    const pin = parsed.pin;
+    console.log('Instruction page for player in room:', pin);
+    this.setState({
+      nickname: nickname,
+      pin: pin
+    })
+
+    socket.on(GAME_HAS_STARTED, () => {
+      this.props.history.push(`/getready?nickname=${ this.state.nickname }&pin=${ this.state.pin }`)
+   })
+  }
+
+  render() {
+    return (
+      <div>
+        <Pin pin={ this.state.pin }/>
+        <p>You're in</p>
+        <p>See your nickname on screen?</p>
+      </div>
+    )
+  }
 }

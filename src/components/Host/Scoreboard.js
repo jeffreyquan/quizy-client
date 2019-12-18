f21c2db6aa771c81e2d511c77f128dd2ad118ff1
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-19 01:23:10 +1100

    displaying final scoreboard -- can change this to functional component

--- src/components/Host/Scoreboard.js
+++ src/components/Host/Scoreboard.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
 import { socket } from '../Global/Header';
+import Button from '@material-ui/core/Button';
+import { FINISH_GAME } from '../Events';
 
 export default class Scoreboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  finishGame = () => {
+    this.props.endGame();
+  }
+
+  handleClick = () => {
+    this.props.nextQuestion();
+  }
 
   render () {
+    let button;
+    console.log('Game status is: ', this.props.gameStatus);
+    if (this.props.gameStatus) {
+      button = <Button variant="contained" color="primary" onClick={ this.handleClick }>Next</Button>
+    } else {
+      button = <Button variant="contained" color="primary" onClick={ this.finishGame }>End</Button>
+    }
     return (
       <div>
-        Scoreboard coming soon
+        <h1>Scorboard</h1>
+        <Rankings playerRanks={ this.props.rankedPlayers } />
+        { button }
       </div>
     )
   }
+}
+
+const Rankings = (props) => {
+
+  const playerRankings = props.playerRanks.map((r, i) => (
+    <div key={ i }>
+      { r.rank } - { r.nickname } - { r.score }
+    </div>
+  ))
+
+  return (
+    <div>
+      { playerRankings }
+    </div>
+  )
 }

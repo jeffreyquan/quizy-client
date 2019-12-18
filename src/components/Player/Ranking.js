94af9feabbc6c61b412685868197475eb63534c3
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-18 16:43:53 +1100

    initiated ranking component

---
+++ src/components/Player/Ranking.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { socket } from '../Global/Header';
+
+export default class Ranking extends Component {
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        Ranking coming soon
+      </div>
+    )
+  }
+}

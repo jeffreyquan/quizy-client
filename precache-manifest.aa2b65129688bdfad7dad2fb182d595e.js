af347d7d60caa4c4eab20c3e529e0936ee906385
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-20 10:13:14 +1100

    Updates

---
+++ precache-manifest.aa2b65129688bdfad7dad2fb182d595e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8ede0108aa523c8e5558e57ccdb88a",
+    "url": "/quizy/index.html"
+  },
+  {
+    "revision": "b60c68530d402be69c79",
+    "url": "/quizy/static/css/main.f67b032f.chunk.css"
+  },
+  {
+    "revision": "df1d6bba1e59990506f6",
+    "url": "/quizy/static/js/2.16a2cb5d.chunk.js"
+  },
+  {
+    "revision": "57a3ddcc24132d62e859a340705dec12",
+    "url": "/quizy/static/js/2.16a2cb5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b60c68530d402be69c79",
+    "url": "/quizy/static/js/main.6a466f13.chunk.js"
+  },
+  {
+    "revision": "39ec0c199e19c38f0eff",
+    "url": "/quizy/static/js/runtime-main.bd469de2.js"
+  },
+  {
+    "revision": "bcd504e359ac85da186824191425d883",
+    "url": "/quizy/static/media/theme.bcd504e3.mp3"
+  }
+]);

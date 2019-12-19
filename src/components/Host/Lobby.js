f27343ff99949833b6d1418bd3e01c999f025d3e
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-19 18:05:20 +1100

    styling to lobby

--- src/components/Host/Lobby.js
+++ src/components/Host/Lobby.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import './Lobby.css';
 import { Link } from 'react-router-dom';
 import Pin from '../Global/Pin';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import { socket } from '../Global/Header';
 import { HOST_JOINED, SHOW_PIN, UPDATE_PLAYERS_IN_LOBBY, HOST_STARTED_GAME } from '../Events';
@@ -11,7 +13,8 @@ export default class Lobby extends Component {
     this.state = {
       quizId: '',
       pin: null,
-      players: []
+      players: [],
+      playersCount: 0
     };
   }
 
@@ -32,9 +35,10 @@ export default class Lobby extends Component {
       })
     })
 
-    socket.on(UPDATE_PLAYERS_IN_LOBBY, players => {
+    socket.on(UPDATE_PLAYERS_IN_LOBBY, playersData => {
       this.setState({
-        players: players
+        players: playersData.players,
+        playersCount: playersData.playersCount
       })
     })
   }
@@ -51,14 +55,72 @@ export default class Lobby extends Component {
 
   render() {
     return (
-      <div>
-        <Pin pin={ this.state.pin } />
-        <Players players={ this.state.players }/>
-        <Link to={`/start?quizId=${ this.state.quizId }&pin=${ this.state.pin }`}>
-          <Button variant="contained" color="primary" onClick={ this.startGame }>
-            Start
-          </Button>
-        </Link>
+      <div className="main">
+        <Grid
+          container
+          direction="column"
+        >
+          <Grid
+            item
+            xs={12}
+            style={{ minHeight: "20vh" }}
+          >
+            <div className="title">
+              <h1 className="join">Join with Game PIN: </h1>
+              <h1 className="pin">{ this.state.pin }</h1>
+            </div>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            style={{ marginTop: "20px" }}
+          >
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+              alignItems="center"
+              style={{ minHeight: "10vh" }}
+            >
+              <Grid
+                item
+                xs={4}
+                style={{ paddingLeft: "50px" }}
+              >
+                <div className="players-count">
+                  { this.state.playersCount } players
+                </div>
+              </Grid>
+              <Grid
+                item
+                xs={4}
+                style={{ textAlign: "center" }}
+              >
+                <h1 className="logo">QUIZY</h1>
+              </Grid>
+              <Grid
+                item
+                xs={4}
+                style={{ textAlign: "right", paddingRight: "50px" }}
+              >
+                <Link to={`/start?quizId=${ this.state.quizId }&pin=${ this.state.pin }`}>
+                  <Button variant="contained" color="primary" className="start-btn" onClick={ this.startGame }>
+                    Start
+                  </Button>
+                </Link>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+          >
+            <Players players={ this.state.players }/>
+          </Grid>
+          <Grid>
+
+          </Grid>
+        </Grid>
       </div>
     )
   }

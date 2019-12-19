e65d6e0dece6991192757947113d1854061a3f73
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-19 18:08:21 +1100

    styling to home page

--- src/components/Player/JoinGame.js
+++ src/components/Player/JoinGame.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './JoinGame.css';
+import { MuiThemeProvider, createMuiTheme, withStyles  } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import { socket } from '../Global/Header';
 import { PLAYER_JOINED, GAME_NOT_FOUND, PLAYER_JOINED_SUCCESSFULLY } from '../Events';
+
+const darkGreyTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: grey[900]
+    }
+  }
+})
 
 export default class JoinGame extends Component {
   constructor() {
@@ -10,7 +23,8 @@ export default class JoinGame extends Component {
     this.state = {
       nickname: null,
       pin: null,
-      message: null
+      message: null,
+      disabled: false
     };
   }
 
@@ -20,6 +34,12 @@ export default class JoinGame extends Component {
       [name]: value
     });
   };
+
+  handleClick = event => {
+    this.setState({
+      disabled: true
+    })
+  }
 
   handleSubmit = event => {
     event.preventDefault();
@@ -54,16 +74,76 @@ export default class JoinGame extends Component {
       error = <div>We didn't recognise the game pin. Please check and try again.</div>
     }
     return (
-      <div>
-        <p>Making sure works</p>
-        <form onSubmit={ this.handleSubmit }>
-          <TextField label="Nickname" name="nickname" value={ this.state.nickname || '' } onChange={ this.handleChange } margin="dense" variant="filled" required/>
-          <TextField label="Game pin" name="pin" value={ this.state.pin || '' } onChange={ this.handleChange } margin="dense" variant="filled" required/>
-          <Button variant="contained" color="primary" type="submit">
-            Enter
-          </Button>
-        </form>
-        { error }
+      <div className="home">
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: '100vh' }}
+        >
+          <Grid item lg={2} md={3} xs={5}>
+            <h1 className="main-title">QUIZY</h1>
+          </Grid>
+          <Grid item lg={2} md={3} xs={5}>
+            <form onSubmit={ this.handleSubmit }>
+              <TextField
+                inputProps={{
+                  style: {
+                    textAlign: "center",
+                    fontWeight: "bold",
+                    color: "black"
+                  }
+                }}
+                placeholder="NICKNAME"
+                name="nickname"
+                value={ this.state.nickname || '' }
+                onChange={ this.handleChange }
+                margin="dense"
+                variant="outlined"
+                required fullWidth
+              />
+              <TextField
+                inputProps={{
+                  style: {
+                    textAlign: "center",
+                    fontWeight: "bold",
+                    color: "black"
+                  }
+                }}
+                placeholder="GAME PIN"
+                name="pin"
+                value={ this.state.pin || '' }
+                onChange={ this.handleChange }
+                margin="dense"
+                variant="outlined"
+                required fullWidth
+              />
+              <MuiThemeProvider theme={ darkGreyTheme }>
+                <Button
+                  style={{
+                    fontWeight: "bold"
+                  }}
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  disabled={ this.state.disabled }
+                  onClick={ this.handleClick }
+                  fullWidth
+                >
+                  Enter
+                </Button>
+              </MuiThemeProvider>
+            </form>
+          </Grid>
+          <Grid item lg={2} md={3} xs={5}>
+            { error }
+          </Grid>
+          <Grid item lg={2} md={3} xs={5}>
+            <p style={{ color: "white" }}>Create your own quiz <Link to="/quizzes/new" style={{ color: "white", textDecoration: "none", fontWeight: "bold" }}>here</Link>.</p>
+          </Grid>
+        </Grid>
       </div>
     )
   }

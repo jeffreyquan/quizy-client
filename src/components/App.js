f3ec46194b7dcc80d971f26a81b4d759e59dbcbc
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-21 16:15:03 +1100

    adjusted imports to include correct paths

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,12 @@ import Quiz from './Quiz/Quiz';
 import Lobby from './Host/Lobby/Lobby';
 import Start from './Host/Start/Start';
 import Gameblock from './Host/Gameblock/Gameblock';
-import JoinGame from './Player/JoineGame/JoinGame';
+import JoinGame from './Player/JoinGame/JoinGame';
 import QuizForm from './Quiz/NewQuiz/QuizForm/QuizForm';
 import Playblock from './Player/Playblock/Playblock';
 import Instructions from './Player/Instructions/Instructions';
 import GetReady from './Player/GetReady/GetReady';
-import Header from './Global/Header';
+import { Header } from './Global/Header';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
 function App() {

b1af4f2f9472a9938049c3c336f5b49f8d7aea72
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-24 01:29:46 +1100

    imported newly installed fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-montserrat';
+import 'typeface-acme';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';

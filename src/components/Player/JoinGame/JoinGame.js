c3718d39e18f080ab147d70788b0cdba26f85bf0
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-23 23:17:42 +1100

    converted scss files to module scss files to locally scope scss

--- src/components/Player/JoinGame/JoinGame.js
+++ src/components/Player/JoinGame/JoinGame.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import './JoinGame.scss';
 import { MuiThemeProvider, createMuiTheme, withStyles  } from '@material-ui/core/styles';
 import InputBase from '@material-ui/core/InputBase';

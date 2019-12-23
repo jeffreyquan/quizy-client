7982d50541ea400cf6630257844f65d9e9b47bc9
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-24 02:22:55 +1100

    created separate footer shared between several components

---
+++ src/components/Host/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styles from './Footer.module.scss';
+import Grid from '@material-ui/core/Grid';
+
+export default function Footer(props) {
+  return (
+    <Grid
+      item
+      container
+      alignItems="center"
+      xs={12}
+      style={{ minHeight: "10vh" }}
+      className={ styles.footer }
+    >
+      <div style={{ paddingRight: "1rem" }}><strong>jeffreyquan.github.io/quizy-client</strong></div>
+      <div>Game PIN: <strong>{ props.pin }</strong></div>
+    </Grid>
+  )
+}

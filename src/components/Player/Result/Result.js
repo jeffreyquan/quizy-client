b9b3c0abfa36d10d421390f394e4768241bec8da
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-24 01:32:57 +1100

    styling to result component for player

--- src/components/Player/Result/Result.js
+++ src/components/Player/Result/Result.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import StatusBar from '../StatusBar/StatusBar';
+import styles from './Result.module.scss';
+import CloseIcon from '@material-ui/icons/Close';
+import CheckIcon from '@material-ui/icons/Check';
 
 export default class Results extends Component {
 
@@ -7,16 +12,32 @@ export default class Results extends Component {
     let showResult;
 
     if (correct) {
-      showResult = <div>Correct</div>
+      showResult =
+      <div>
+        <div>Correct</div>
+        <div>
+          <CheckIcon style={{ color:"white", fontSize: 100 }}/>
+        </div>
+      </div>
     } else {
-      showResult = <div>Incorrect</div>
+      showResult =
+      <div>
+        <div>Incorrect</div>
+        <div>
+          <CloseIcon style={{ color:"white", fontSize: 100 }}/>
+        </div>
+      </div>
     }
 
     const streak = this.props.streak;
     let showStreak;
 
     if (streak > 0) {
-      showStreak = <div>{`Your streak is ${ streak }`}</div>
+      showStreak =
+      <div style={{ display: "flex" }}>
+        <div>Answer streak</div>
+        <div className={ styles.streakNumber }>{ streak }</div>
+      </div>
     } else {
       showStreak = <></>
     }
@@ -33,12 +54,37 @@ export default class Results extends Component {
     } else {
       showRank = <div>{`You are in ${ rank }th place`}</div>
     }
-
+    const { pin, questionNumber, totalNumberOfQuestions } = this.props;
     return (
       <div>
-        <div>{ showResult }</div>
-        <div>{ showStreak }</div>
-        <div>{ showRank }</div>
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: '100vh' }}
+        >
+          <StatusBar
+            pin={ pin }
+            questionNumber="1"
+            totalNumberOfQuestions="5"
+            nickname="jeffrey"
+          />
+          <Grid
+            item
+            container
+            xs={12}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: "85vh", textAlign: "center" }}
+            className={ correct ? styles.correct :  styles.incorrect }
+          >
+            <div className={ styles.result }>{ showResult }</div>
+            <div className={ styles.streak }>{ showStreak }</div>
+            <div className={ styles.rank }>{ showRank }</div>
+          </Grid>
+        </Grid>
       </div>
     )
   }

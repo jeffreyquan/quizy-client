102e75a90cfd86d433a09adb708237c243ea7568
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-12-28 18:50:49 +1100

    fetch all quizzes axios request

--- src/components/utils.js
+++ src/components/utils.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 const QuizInfo = {
   getQuiz(id) {
     return axios.get(`https://quizy-server.herokuapp.com/quizzes/${ id }`)
+  },
+
+  getAllQuizzes() {
+    return axios.get('http://localhost:3000/quizzes')
   }
 }
 
